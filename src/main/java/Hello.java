bd03ce805b35a15c582c574a4b6eb11a262b0cad
zhaochuang <zhaochuang@jszfti.com>
2026-06-25 16:42:24 +0800

    Initial commit: Hello Java CI

---
+++ src/main/java/Hello.java
@@ -0,0 +1,7 @@
+package main.java;
+public class Hello 
+{
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI! (Java version: " + System.getProperty("java.version") + ")");
+    }
+}
